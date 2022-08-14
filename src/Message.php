2529bb52c3c41677d2fd456ce91a57afbf931b6b
Quentin Gabriele <quentin.gabriele@gmail.com>
2022-08-14 17:45:38 +0200

    fix stan

--- src/Message.php
+++ src/Message.php
@@ -2,6 +2,7 @@
 
 namespace Finller\Conversation;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User;
