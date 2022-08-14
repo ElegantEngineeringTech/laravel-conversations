560ac95d5611225315856c834a357ad71265031a
Quentin Gabriele <quentin.gabriele@gmail.com>
2022-08-14 14:52:31 +0200

    add trait

---
+++ src/HasConversation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Finller\Conversation;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+/**
+ * @property ?Conversation $conversation
+ */
+trait HasConversation
+{
+    protected static function bootedHasConversation()
+    {
+        // static::deleting(function (Model $model) {
+        //     $model->conversation()->delete(); // @phpstan-ignore-line
+        // });
+    }
+
+    public function conversation(): MorphOne
+    {
+        return $this->morphOne(Conversation::class, 'conversationable');
+    }
+}
