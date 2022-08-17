c2730f6c7180aba687c6467d39da68284ddf7752
Quentin Gabriele <quentin.gabriele@gmail.com>
2022-08-18 00:21:25 +0200

    fix

--- src/Requests/StoreMessageRequest.php
+++ src/Requests/StoreMessageRequest.php
@@ -4,7 +4,7 @@ namespace Finller\Conversation\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateMessageRequest extends FormRequest
+class StoreMessageRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
