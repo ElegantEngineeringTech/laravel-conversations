c07fe153e09667da9b2a7621d98891e24b349ae4
Quentin Gabriele <quentin.gabriele@polyconseil.fr>
2022-09-24 17:26:07 +0200

    add users prop

--- src/Conversation.php
+++ src/Conversation.php
@@ -2,12 +2,16 @@
 
 namespace Finller\Conversation;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+/**
+ * @property Collection $users
+ */
 class Conversation extends Model
 {
     use HasFactory;
